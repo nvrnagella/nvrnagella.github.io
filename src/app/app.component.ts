ea7701c2a9f18814128730d0184987525c8dee28
Venkat <Venkat@TECH-10-VENKAT>
2026-02-15 16:53:16 +0530

    improved documentation site experience like angular dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,57 @@ import { DOCS_CONFIG } from './config/docs.config';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-   // add open state to every group
+
   docs = DOCS_CONFIG.map(group => ({
     ...group,
-    open: true   // default expanded
+    open: true
   }));
+
+  // ===== Sidebar State =====
+  sidebarOpen = false;
 
   toggle(group: any) {
     group.open = !group.open;
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+
+    document.body.classList.toggle(
+      'menu-open',
+      this.sidebarOpen
+    );
+  }
+
+
+  // ===== Swipe Gesture Support =====
+  touchStartX = 0;
+  touchEndX = 0;
+
+  onTouchStart(event: TouchEvent) {
+    this.touchStartX = event.touches[0].clientX;
+  }
+
+  onTouchMove(event: TouchEvent) {
+    this.touchEndX = event.touches[0].clientX;
+  }
+
+  onTouchEnd() {
+
+    const swipeDistance = this.touchEndX - this.touchStartX;
+
+    // ✅ OPEN sidebar (swipe right from edge)
+    if (
+      window.innerWidth <= 900 &&
+      this.touchStartX < 40 &&
+      swipeDistance > 80
+    ) {
+      this.sidebarOpen = true;
+    }
+
+    // ✅ CLOSE sidebar (swipe left)
+    if (this.sidebarOpen && swipeDistance < -80) {
+      this.sidebarOpen = false;
+    }
   }
 }
